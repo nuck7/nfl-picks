f3069d0814b5b3a980956da29aca895c48b81ea6
nuck7 <chu071@gmail.com>
2023-09-04 19:24:03 -0700

    Fix dropdowns picks form, add week id on submit (#7)
    Co-authored-by: Nick Chu <nuck@Nucks-Laptop.local>

--- src/components/PickForm/index.tsx
+++ src/components/PickForm/index.tsx
@@ -4,20 +4,16 @@ import { EspnCompetitor, EspnMatchup, Matchup, MatchupV2, Picks, Team, Week } fr
 import { useLocation } from 'react-router-dom';
 import { Form } from 'grommet';
 import { pickDbToForm } from '../../utils/form';
-import CustomFormField from '../CustomFormField';
 import { AtContainer, MatchupLabel, PickContainer, StyledFormField, TeamSelectContainer, SubmitButton } from './index.styles';
-import { emptyPickFormState, emptyPickFormStateV2, emptyWeekFormState } from '../../constants';
+import { emptyPickFormStateV2 } from '../../constants';
 import { collection, getDocs, query } from 'firebase/firestore';
 import { db } from '../../resources/firebase.config';
 import { getDocuments } from '../../resources/firebase';
-import { getCurrentWeekMatchups } from '../../resources/espn';
+import { getCurrentWeekId, getCurrentWeekMatchups } from '../../resources/espn';
 
 const PicksForm = () => {
     const [value, setValue] = useState({});
-    const [teams, setTeams] = useState<Team[]>([]);
-    const [weekId, setWeekId] = useState<string>('');
-    const [weeks, setWeeks] = useState<Week[]>([]);
-    const [week, setWeek] = useState<Week>();
+    const [weekId, setWeekId] = useState<number>();
     const [matchups, setMatchups] = useState<EspnMatchup[]>([]);
     const [picks, setPicks] = useState<any>();
 
@@ -25,11 +21,9 @@ const PicksForm = () => {
     const pathMatch = pathname.match(/\/week\/(\d|5)$/);
     const weekIdPath = pathMatch ? pathMatch[1] : pathMatch;
 
-    if (weekIdPath != null) {
-        setWeekId(weekIdPath)
-    }
-    const teamCollectionRef = collection(db, 'teams')
-    const weekCollectionRef = collection(db, 'weeks')
+    // if (weekIdPath != null) {
+    //     setWeekId(weekIdPath)
+    // }
     const matchupCollectionRef = collection(db, 'matchups')
     const pickCollectionRef = collection(db, 'picks')
 
@@ -39,7 +33,6 @@ const PicksForm = () => {
     useMemo(() => {
         const fetchMatchups = async () => {
             const matchups = await getCurrentWeekMatchups()
-            console.log(`matchups ${JSON.stringify(matchups)}`)
             setMatchups(matchups)
         }
         fetchMatchups()
@@ -57,111 +50,43 @@ const PicksForm = () => {
                 weekId: data.weekId,
                 matchups: data.matchups,
             }
-            // console.log(`picks ${JSON.stringify(picks)}`)
             setPicks(picks)
         }
         fetchPicks()
     }, [])
 
-    // useMemo(() => {
-    //     const fetchTeams = async () => {
-    //         const q = query(teamCollectionRef)
-    //         const querySnapshot = await getDocs(q)
-    //         const teams = querySnapshot.docs.map((doc) => {
-    //             const data = doc.data();
-    //             return {
-    //                 id: doc.id,
-    //                 name: data.name,
-    //                 city: data.city,
-    //             }
-    //         })
-    //         console.log(`Teams ${teams}`)
-    //         setTeams(teams)
-    //     }
+    useMemo(async() => {
+        const weekId = await getCurrentWeekId()
+        console.log(`weekId ${weekId}`)
+        setWeekId(weekId)
+        const state = formState
+        state.week_id = weekId
+        setFormState(formState)
+    }, [matchups])
 
-    //     fetchTeams()
-    // }, [])
-
-    useEffect(() => {
-        const fetchCurrentWeek = async () => {
-            const q = query(weekCollectionRef)
-            const querySnapshot = await getDocs(q)
-            const week = querySnapshot.docs[0]
-            if (week) {
-                const data = week.data();
-                setWeek({
-                    id: week.id,
-                    name: data.name,
-                    start: data.start,
-                    end: data.end,
-                })
+    const getMatchupOptions = (matchup: EspnMatchup, teamNames: string[]) => {
+        return matchup.competitions[0].competitors.map((competitor: EspnCompetitor) => {
+            return {
+                name: competitor.homeAway == 'away' ? teamNames[0] : teamNames[1],
+                id: parseInt(competitor.id)
             }
-        }
-        fetchCurrentWeek()
-    }, [])
-
-    // useEffect(() => {
-    //     const fetchMatchups = async () => {
-    //         // const q = query(matchupCollectionRef)
-    //         // const querySnapshot = await getDocs(q)
-    //         const docs = await getDocuments(matchupCollectionRef)
-    //         console.log(`docs ${JSON.stringify(docs)}`)
-
-    //         const matchups = docs.map((doc) => {
-    //             // let data = doc.data();                
-    //             let matchup = doc.data();
-    //             // matchup.id = matchup._document.key.path.segments;
-    //             // console.log(`Matchups length ${JSON.stringify(_document._key)}`)
-
-    //             // const q = query(matchupCollectionRef)
-    //             // const querySnapshot = await getDocs(q)
-    //             // const matchups = querySnapshot.docs.map((doc) => {
-    //             //     matchup.pickRef.get()
-    //             //         .then((res: { data: () => any; }) => {
-    //             //             matchup.weekID = res.data()
-    //             //         })
-    //             //         .catch((err: any) => console.error(err));
-    //             // }
-    //             console.log(`Matchup ${JSON.stringify({
-    //                 id: doc.id,
-    //                 weekID: matchup.weekID._key.path.segments[6],
-    //                 homeTeamID: matchup.homeTeamID._key.path.segments[6],
-    //                 awayTeamID: matchup.awayTeamID._key.path.segments[6],
-    //             })}`)
-    //             return {
-    //                 id: doc.id,
-    //                 weekID: matchup.weekID._key.path.segments[6],
-    //                 homeTeamID: matchup.homeTeamID._key.path.segments[6],
-    //                 awayTeamID: matchup.awayTeamID._key.path.segments[6],
-    //             }
-    //         })
-    //         console.log(`Matchups ${JSON.stringify(matchups)}`)
-
-    //         setMatchups(matchups)
-    //     }
-    //     fetchMatchups()
-    // }, [week])
+        })
+    }
 
     return (
         <Form
             value={value}
             onChange={onChange}
             onSubmit={async () => {
-                console.log("Submit", formState)
+                console.log("Submit", JSON.stringify(formState))
                 // const submission = weekId ? await submitUpdatePicks(formState as any, parseInt(weekId)) : await submitCreatePicks(formState)
-
                 // alert(`Response: ${JSON.stringify(submission)}`)
             }}
             onReset={() => setValue({})}
         >
             {matchups ? matchups.map((matchup, index: any) => {
                 const teamNames = matchup.name.split(' at ')
-                const options = matchup.competitions[0].competitors.map((competitor: EspnCompetitor) => {
-                    return {
-                        name: competitor.homeAway == 'away' ? teamNames[0] : teamNames[1],
-                        id: competitor.id
-                    }
-                })
+                const options = getMatchupOptions(matchup, teamNames)
 
                 return (
                     <PickContainer key={`matchup_cont_${index}`}>
@@ -169,25 +94,30 @@ const PicksForm = () => {
                             {`${matchup?.name}`}
                         </MatchupLabel>
                         <TeamSelectContainer>
-                            <StyledFormField name={`matchup_${index}_away`} label={"Winning Team"}>
+                            <StyledFormField name={`matchup_${index}_winning_team`} label={"Winning Team"}>
                                 <SelectField
                                     id={`matchup_${index}`}
                                     label="Picked Team"
-                                    name={`matchup_${index}`}
+                                    name={`matchup ${index+1}`}
                                     options={options}
                                     value={formState?.picks[index]?.pickedTeam}
                                     defaultValue={formState?.picks[index]?.pickedTeam}
                                     onChange={event => {
                                         let state = formState
-                                        state.picks[index].pickedTeam.id = event.value
-                                        state.picks[index].pickedTeam.name = event.label
-                                        console.log(`event value ${JSON.stringify(event.value)}`)
-                                        console.log(`state ${JSON.stringify(state)}`)
+                                        for (const team of matchup.competitions[0].competitors) {
+                                            if (team.homeAway == 'home') {
+                                                state.picks[index].homeTeam.id = parseInt(team.id)
+                                                state.picks[index].homeTeam.name = teamNames[1]
+                                            }
+                                            if (team.homeAway == 'away') {
+                                                state.picks[index].awayTeam.id = parseInt(team.id)
+                                                state.picks[index].awayTeam.name = teamNames[0]
+                                            }
+                                        }
+                                        state.picks[index].pickedTeam = event.value
                                         setFormState(state)
                                     }}
-                                    labelKey={(option) => (
-                                        `${option.name}`
-                                    )}
+                                    labelKey="name"
                                     valueKey="id"
                                 />
                             </StyledFormField>
